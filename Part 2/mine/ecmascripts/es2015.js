4f1a46df8309d111e331655acd05e5e2bb9a7a58
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2024-09-19 19:38:31 +0530

    New things add

---
+++ Part 2/mine/ecmascripts/es2015.js
@@ -0,0 +1,15 @@
+//! ==========================================
+//!  Part 2 of Modern JavaScript
+//! =========================================
+
+//? what we will learn in this.
+// i. Object Properties
+
+
+//* =========================================
+//*  Object Properties - Modern JavaScript
+//* =========================================
+// const name = "vinod";
+// const age = 30;
+
+//? traditional way
